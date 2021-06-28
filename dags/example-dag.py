38a6f4f40eccaf1fcc793eedd8f9664014849422
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2021-06-28 11:27:02 -0400

    Add something to the example DAG

--- dags/example-dag.py
+++ dags/example-dag.py
@@ -14,6 +14,7 @@ def my_custom_function(ts,**kwargs):
     print(f"I am task number {kwargs['task_number']}. This DAG Run execution date is {ts} and the current time is {datetime.now()}")
     print('Here is the full DAG Run context. It is available because provide_context=True')
     print(kwargs)
+    print("hello")
 
 
 # Default settings applied to all tasks
